2d9a832696940b39b943899f501c579f8064fc22
sledziokrator <throwawaysledz@gmail.com>
2022-03-02 22:43:53 +0100

    poprawka

--- old/licznikchujow.py
+++ old/licznikchujow.py
@@ -1,5 +1,5 @@
 ile=int(input ('Jak bardzo chuj?: '))
-
+import os
 import time
 t0=time.time()
 i=1
@@ -8,6 +8,6 @@ for i in range(ile):
  t1=time.time()
 print('i juz nie chuj. Przeliczylem wszystkie te chuje w ' + str(round(t1-t0, 2)) + ' sekund')
 if input('Czyscic?: ').upper()=='T':
- print('\033c')
+ os.system('clear')
 else:
  print('no ok')
