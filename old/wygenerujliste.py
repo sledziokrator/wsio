95175cf2b9824001e7362842abb62b103196b5e2
sledziokrator <throwawaysledz@gmail.com>
2022-04-04 22:09:14 +0200

    ok

---
+++ old/wygenerujliste.py
@@ -0,0 +1,8 @@
+import string
+
+alf=list(string.ascii_uppercase)
+num=[pozycja for pozycja,litera in enumerate(alf,1)]
+spec=[i for i in string.punctuation[:len(alf)]]
+
+l1=[list(i) for i in zip(num,alf,spec)]
+print(l1)
